9ec3a224c4b4510f7cd8d87455a6a96d4e80ede1
KHalil Touil <touikhalil2004s@gmail.com>
2024-06-19 01:54:43 +0200

    Updated feature

--- src/App.js
+++ src/App.js
@@ -7,13 +7,7 @@ function App() {
   const [feedbackMessage1, setFeedbackMessage1] = useState('');
   const [feedbackMessage2, setFeedbackMessage2] = useState('');
   const [feedbackMessage3, setFeedbackMessage3] = useState('');
-  const click = () => {
-    setFeedbackMessage1 ('')
-    setFeedbackMessage2 ('')
-    setFeedbackMessage3 ('')
 
-
-  };
 
   const form = useRef();
 
@@ -21,15 +15,17 @@ function App() {
     e.preventDefault();
 
     emailjs
-      .sendForm('service_vm0b4jv', 'template_31du4kd', form.current, {
-        publicKey: '7uLuNhvRf_h_iZGiD',
-      })
+      .sendForm(process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_TEMPLATE_ID,
+        form.current,
+        process.env.REACT_APP_PUBLIC_KEY)
       .then(
         () => {
           console.log('SUCCESS!');
           alert('Email sent successfully!');
-          click()
-        },
+          setFeedbackMessage1('');
+          setFeedbackMessage2('');
+          setFeedbackMessage3('');        },
         (error) => {
           console.log('FAILED...', error.text);
           alert('Failed to send email. Please try again later.');
@@ -45,7 +41,7 @@ function App() {
       <input type="text" name="user_name" placeholder='Your Name ' value={feedbackMessage1} onChange={(e)=>{setFeedbackMessage1(e.target.value)}}/>
       <input type="email" name="user_email" placeholder='Your Email' value={feedbackMessage2} onChange={(e)=>{setFeedbackMessage2(e.target.value)}}/>
       <textarea name="message" placeholder='Your Text' className='textarea' value={feedbackMessage3} onChange={(e)=>{setFeedbackMessage3(e.target.value)}}/>
-      <input type="submit" value="Send"  className='button' onClick={click}/>
+      <input type="submit" value="Send"  className='button' />
     </form>
       </header>
    
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom';
+import './index.css';  // Import your global CSS styles
+import App from './App';  // Import your main App component
 import reportWebVitals from './reportWebVitals';
 
+// Load environment variables from .env file
+
+// Initialize ReactDOM
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render your App component inside a React.StrictMode for better development practices
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Optional: Measure and log performance metrics
 reportWebVitals();
